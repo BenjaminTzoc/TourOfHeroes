6444ec526c630ed8e16afd072e4b09294d90bedb
BenjaminTzoc <jtzocb@gmail.com>
2022-06-22 15:44:44 -0600

    Se agrega el proyecto

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Elise'},
+  {id: 12, name: 'Nidalee'},
+  {id: 13, name: 'Warwick'},
+  {id: 14, name: 'Akali'},
+  {id: 15, name: 'Azir'},
+  {id: 16, name: 'Diana'},
+  {id: 17, name: 'Vi'},
+  {id: 18, name: 'Kindred'},
+  {id: 19, name: 'Udyr'},
+  {id: 20, name: 'Ornn'},
+]
